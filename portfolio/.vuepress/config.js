3a3f962042f1174db1b9a24706ecbebef0377d66
kkrosefields <kkrosefields@gmail.com>
2018-08-31 07:44:37 -0700

    first commit

---
+++ portfolio/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: "Acme",
+  description: "creative agency",
+  base: "/",
+  themeConfig: {
+    logo: "/upload/logo.svg",
+    footer: "Ⓒ Acme",
+    nav: [
+      { text: "Works", link: "/", position: "left", external: false },
+      { text: "Instagram", link: "https://www.instagram.com/its.nwa/", position: "left", external: true },
+      { text: "Say hi!", link: "mailto:sayhi@mydomain.com", position: "right", external: true },
+      { text: 'Journal', link: '/journal/', position: 'right', external: false },
+    ]
+  },
+  head: [
+    ['link', { rel: "icon", href: "favicon-32x32.png" }]
+  ],
+  markdown: {
+    anchor: {
+      permalink: false
+    }
+  }
+};
